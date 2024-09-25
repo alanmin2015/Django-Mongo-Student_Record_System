82b6549ba4375fe1196c41e6edcbc45803c68834
Alan Min <84518355+alanmin2015@users.noreply.github.com>
2024-09-24 23:19:08 -0400

    Vercel setting

--- studentrecordsystem/settings.py
+++ studentrecordsystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e_s%u7msavh*-%(!#q5xs(&og5t6rp2u3zr3u%uaz$j3q^g+&#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
